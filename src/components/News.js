d80912542ac6aae356153cd026e2cd6413aca598
World-Pirate <paragkshirsagar.cg@gmail.com>
2023-05-03 01:16:12 +0530

    Destructuring props in news item

--- src/components/News.js
+++ src/components/News.js
@@ -5,11 +5,20 @@ export class News extends Component {
   render() {
     return (
       <div>
-        This is our News component
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
+        <div className="container my-3">
+            <h2>News Spider - Top Headlines</h2>
+            <div className="row">
+                <div className="col-md-4">
+                    <NewsItem title='mytitle' description='mydescription' imageUrl="https://i.ytimg.com/vi/OmoRSF1Ta6A/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLBIrzwsEadXR3vvauNkTzj52HaIsw"></NewsItem>
+                </div>
+                <div className="col-md-4">
+                    <NewsItem title='mytitle' description='mydescription'></NewsItem>
+                </div>
+                <div className="col-md-4">
+                    <NewsItem title='mytitle' description='mydescription'></NewsItem>
+                </div>
+            </div>
+        </div>
       </div>
     )
   }
