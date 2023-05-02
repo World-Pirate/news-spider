e623182273fa3d32bfdb7b833628b72a309256ff
World-Pirate <paragkshirsagar.cg@gmail.com>
2023-05-02 21:10:59 +0530

    News Spider
    News spider is a news app which can be used to grab quick daily news bites. If you are interested in news about weather, politics and sports then News Spider is for you!

---
+++ src/components/NewsItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    return (
+      <div>
+        This is our News item
+      </div>
+    )
+  }
+}
+
+export default NewsItem
