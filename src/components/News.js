eaebc821bf20252d0f60847c4001219986d86d0c
World-Pirate <paragkshirsagar.cg@gmail.com>
2023-05-03 11:19:13 +0530

    Update News.js

--- src/components/News.js
+++ src/components/News.js
@@ -8,15 +8,25 @@ export class News extends Component {
         <div className="container my-3">
             <h2>News Spider - Top Headlines</h2>
             <div className="row">
+                <div className="col-md-4 my-3">
+                    <NewsItem title='mytitle' description='mydescription' imageUrl="https://i.ytimg.com/vi/OmoRSF1Ta6A/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLBIrzwsEadXR3vvauNkTzj52HaIsw"></NewsItem>
+                </div>
+                <div className="col-md-4 my-3">
+                    <NewsItem title='mytitle' description='mydescription' imageUrl="https://i.ytimg.com/vi/OmoRSF1Ta6A/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLBIrzwsEadXR3vvauNkTzj52HaIsw"></NewsItem>
+                </div>
+                <div className="col-md-4 my-3">
+                    <NewsItem title='mytitle' description='mydescription' imageUrl="https://i.ytimg.com/vi/OmoRSF1Ta6A/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLBIrzwsEadXR3vvauNkTzj52HaIsw"></NewsItem>
+                </div>
+                <div className="col-md-4 my-3">
+                    <NewsItem title='mytitle' description='mydescription' imageUrl="https://i.ytimg.com/vi/OmoRSF1Ta6A/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLBIrzwsEadXR3vvauNkTzj52HaIsw"></NewsItem>
+                </div>
                 <div className="col-md-4">
                     <NewsItem title='mytitle' description='mydescription' imageUrl="https://i.ytimg.com/vi/OmoRSF1Ta6A/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLBIrzwsEadXR3vvauNkTzj52HaIsw"></NewsItem>
                 </div>
                 <div className="col-md-4">
-                    <NewsItem title='mytitle' description='mydescription'></NewsItem>
+                    <NewsItem title='mytitle' description='mydescription' imageUrl="https://i.ytimg.com/vi/OmoRSF1Ta6A/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLBIrzwsEadXR3vvauNkTzj52HaIsw"></NewsItem>
                 </div>
-                <div className="col-md-4">
-                    <NewsItem title='mytitle' description='mydescription'></NewsItem>
-                </div>
+
             </div>
         </div>
       </div>
